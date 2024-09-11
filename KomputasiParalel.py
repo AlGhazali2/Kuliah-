18c44f07c307dc033db0c0004783f05ce37dd8be
Al Ghazali <ahmadzalia6@gmail.com>
2024-09-11 16:45:16 +0700

    Add files via upload
    Project Menampilkan data (perkuliahan komputasi paralel)

---
+++ KomputasiParalel.py
@@ -0,0 +1,13 @@
+'''penjelasan
+pada perkuliahan 11 september 2024,mata kuliah komputasi paralel (pak manda) 
+dijelaskan sebuah skenario dimana kita akan mencari sebuah data dengan parameter tertentu.
+maka dari itu,pada kode kali ini saya akan membuat kode yang otomatis akan mengeluarkan output jika user 
+meminta agar program mencari data dengan parameter tertentu '''
+
+'''pseudocode
+1.user memasukkan data dengan parameter(atau bisa load data tertentu)
+2.program meminta masukkan user "apakah anda ingin mencari data dengan parameter tertentu?" 
+3.jika iya,maka program meminta user untuk memasukkan parameter tertentu
+4.kode pun akan mengeksekusi sesuai masukkan user dan akan menampilkan hasilnya
+5.dengan syarat data hanya berbentuk nested list dan dictionary
+'''
